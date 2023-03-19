d2395e69fbb45c02f2c521ac7dddf3ed7c47339d
mgckaled <marcelgckaled@gmail.com>
2023-03-19 12:16:48 -0300

    feat(Quiz screen): create shake animation to wrong answers

---
+++ src/screens/Quiz/index.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react"
+import { Alert, ScrollView, View } from "react-native"
+import { useNavigation, useRoute } from "@react-navigation/native"
+import Animated, {
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated"
+
+import { styles } from "./styles"
+
+import { QUIZ } from "../../data/quiz"
+import { historyAdd } from "../../storage/quizHistoryStorage"
+
+import { ConfirmButton } from "../../components/ConfirmButton"
+import { Loading } from "../../components/Loading"
+import { OutlineButton } from "../../components/OutlineButton"
+import { Question } from "../../components/Question"
+import { QuizHeader } from "../../components/QuizHeader"
+
+interface Params {
+  id: string
+}
+
+type QuizProps = (typeof QUIZ)[0]
+
+export function Quiz() {
+  const [points, setPoints] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps)
+  const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null)
+
+  const shake = useSharedValue(0)
+
+  const { navigate } = useNavigation()
+
+  const route = useRoute()
+  const { id } = route.params as Params
+
+  function handleSkipConfirm() {
+    Alert.alert("Pular", "Deseja realmente pular a questão?", [
+      { text: "Sim", onPress: () => handleNextQuestion() },
+      { text: "Não", onPress: () => {} },
+    ])
+  }
+
+  async function handleFinished() {
+    await historyAdd({
+      id: new Date().getTime().toString(),
+      title: quiz.title,
+      level: quiz.level,
+      points,
+      questions: quiz.questions.length,
+    })
+
+    navigate("finish", {
+      points: String(points),
+      total: String(quiz.questions.length),
+    })
+  }
+
+  function handleNextQuestion() {
+    if (currentQuestion < quiz.questions.length - 1) {
+      setCurrentQuestion(prevState => prevState + 1)
+    } else {
+      handleFinished()
+    }
+  }
+
+  async function handleConfirm() {
+    if (alternativeSelected === null) {
+      return handleSkipConfirm()
+    }
+
+    if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setPoints(prevState => prevState + 1)
+    } else {
+      shakeAnimation()
+    }
+
+    setAlternativeSelected(null)
+
+    handleNextQuestion()
+  }
+
+  function handleStop() {
+    Alert.alert("Parar", "Deseja parar agora?", [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        style: "destructive",
+        onPress: () => navigate("home"),
+      },
+    ])
+
+    return true
+  }
+
+  function shakeAnimation() {
+    shake.value = withSequence(
+      withTiming(3, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    )
+  }
+
+  const shakeStyleAnimated = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: interpolate(
+            shake.value,
+            [0, 0.5, 1, 1.5, 2, 2.5, 0],
+            [0, -15, 0, 15, 0, -15, 0]
+          ),
+        },
+      ],
+    }
+  })
+
+  useEffect(() => {
+    const quizSelected = QUIZ.filter(item => item.id === id)[0]
+    setQuiz(quizSelected)
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.question}
+      >
+        <QuizHeader
+          title={quiz.title}
+          currentQuestion={currentQuestion + 1}
+          totalOfQuestions={quiz.questions.length}
+        />
+
+        <Animated.View style={shakeStyleAnimated}>
+          <Question
+            key={quiz.questions[currentQuestion].title}
+            question={quiz.questions[currentQuestion]}
+            alternativeSelected={alternativeSelected}
+            setAlternativeSelected={setAlternativeSelected}
+          />
+        </Animated.View>
+
+        <View style={styles.footer}>
+          <OutlineButton title='Parar' onPress={handleStop} />
+          <ConfirmButton onPress={handleConfirm} />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
