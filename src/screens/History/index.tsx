2ee59cb15064e432f99c5a3fda885bc67477dbe3
mgckaled <marcelgckaled@gmail.com>
2023-03-20 17:45:59 -0300

    feat(History screen): use swipeable menu

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,16 +1,18 @@
-import { useNavigation } from "@react-navigation/native"
-import { HouseLine } from "phosphor-react-native"
 import { useEffect, useState } from "react"
-import { Alert, ScrollView, TouchableOpacity, View } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { View, ScrollView, Alert, Pressable } from "react-native"
+import { HouseLine, Trash } from "phosphor-react-native"
 import Animated, { Layout, SlideInRight, SlideOutRight } from "react-native-reanimated"
+import { Swipeable } from "react-native-gesture-handler"
+
+import { historyGetAll, historyRemove } from "../../storage/quizHistoryStorage"
 
 import { Header } from "../../components/Header"
 import { HistoryCard, HistoryProps } from "../../components/HistoryCard"
 import { Loading } from "../../components/Loading"
 
-import { historyGetAll, historyRemove } from "../../storage/quizHistoryStorage"
-
 import { styles } from "./styles"
+import { THEME } from "../../styles/theme"
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true)
@@ -68,9 +70,17 @@ export function History() {
             exiting={SlideOutRight}
             layout={Layout.springify()}
           >
-            <TouchableOpacity onPress={() => handleRemove(item.id)}>
+            <Swipeable
+              overshootLeft={false}
+              containerStyle={styles.swipeableContainer}
+              renderLeftActions={() => (
+                <Pressable style={styles.swipeableRemove}>
+                  <Trash size={32} color={THEME.COLORS.GREY_100} />
+                </Pressable>
+              )}
+            >
               <HistoryCard data={item} />
-            </TouchableOpacity>
+            </Swipeable>
           </Animated.View>
         ))}
       </ScrollView>
--- src/screens/History/styles.ts
+++ src/screens/History/styles.ts
@@ -11,4 +11,19 @@ export const styles = StyleSheet.create({
   history: {
     padding: 32,
   },
+  swipeableContainer: {
+    width: "100%",
+    height: 90,
+    marginBottom: 12,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    borderRadius: 6,
+  },
+  swipeableRemove: {
+    width: 90,
+    height: 90,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 })
