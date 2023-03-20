f7658c57d8ee96112d9f7afe03d237f5e0c707ba
mgckaled <marcelgckaled@gmail.com>
2023-03-20 17:33:10 -0300

    chore: install end config RN Gesture Handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { StatusBar } from "react-native"
-
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto"
+import { StatusBar } from "react-native"
+import "react-native-gesture-handler"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 import { Routes } from "./src/routes"
 
@@ -14,10 +15,10 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
 
       <Routes />
-    </>
+    </GestureHandlerRootView>
   )
 }
