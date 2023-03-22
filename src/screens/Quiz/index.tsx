87f7d77d0760f3d140fda663a873bc228c0394ff
mgckaled <marcelgckaled@gmail.com>
2023-03-22 09:09:02 -0300

    feat(Quiz screen): conclude animated Header

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { Alert, Text, View } from "react-native"
 import Animated, {
   Easing,
+  Extrapolate,
   interpolate,
   useAnimatedScrollHandler,
   useAnimatedStyle,
@@ -138,9 +139,22 @@ export function Quiz() {
     return {
       position: "absolute",
       paddingTop: 50,
+      zIndex: 1,
       backgroundColor: THEME.COLORS.GREY_500,
       width: "110%",
       left: "-5%",
+      opacity: interpolate(scrollY.value, [50, 90], [0, 10], Extrapolate.CLAMP),
+      transform: [
+        {
+          translateY: interpolate(scrollY.value, [50, 100], [-40, 0], Extrapolate.CLAMP),
+        },
+      ],
+    }
+  })
+
+  const headerStyles = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [60, 90], [1, 0], Extrapolate.CLAMP),
     }
   })
 
@@ -167,11 +181,13 @@ export function Quiz() {
         onScroll={scrollHandler}
         scrollEventThrottle={16}
       >
-        <QuizHeader
-          title={quiz.title}
-          currentQuestion={currentQuestion + 1}
-          totalOfQuestions={quiz.questions.length}
-        />
+        <Animated.View style={[styles.header, headerStyles]}>
+          <QuizHeader
+            title={quiz.title}
+            currentQuestion={currentQuestion + 1}
+            totalOfQuestions={quiz.questions.length}
+          />
+        </Animated.View>
 
         <Animated.View style={shakeStyleAnimated}>
           <Question
