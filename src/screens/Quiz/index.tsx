8736577dbbbe2198b95752221efc8a7ec8b21ff2
mgckaled <marcelgckaled@gmail.com>
2023-03-22 11:01:11 -0300

    feat(Quiz screen): apply gesture handler - note gestures

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigation, useRoute } from "@react-navigation/native"
 import { useEffect, useState } from "react"
 import { Alert, Text, View } from "react-native"
+import { Gesture, GestureDetector } from "react-native-gesture-handler"
 import Animated, {
   Easing,
   Extrapolate,
@@ -40,6 +41,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0)
   const scrollY = useSharedValue(0)
+  const cardPosition = useSharedValue(0)
 
   const { navigate } = useNavigation()
 
@@ -158,6 +160,14 @@ export function Quiz() {
     }
   })
 
+  const onPan = Gesture.Pan()
+    .onUpdate(event => {
+      cardPosition.value = event.translationX
+    })
+    .onEnd(() => {
+      cardPosition.value = withTiming(0)
+    })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0]
     setQuiz(quizSelected)
@@ -189,14 +199,16 @@ export function Quiz() {
           />
         </Animated.View>
 
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={shakeStyleAnimated}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title='Parar' onPress={handleStop} />
